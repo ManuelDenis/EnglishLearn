2343f674e102068fb9652943a2b847159ac3217f
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-11-27 22:21:44 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Col, Row} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Homepage from "./Components/Homepage";
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <Container className='text-light'>
+        <Row className="justify-content-md-center p-1">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+      </Routes>
+      </BrowserRouter>
+        </Row>
+    </Container>
+  );
+}
+
+export default App;
