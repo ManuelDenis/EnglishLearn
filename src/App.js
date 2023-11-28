88c6f261d8bce30dee9c501a7b216d295f66e441
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-11-28 19:24:30 +0200

    19

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { Component } from 'react';
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter basename="">
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-
-          <hr />
-
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </HashRouter>
-    );
-  }
+function App ()  {
+  return (
+      <h1>Home</h1>
+  )
 }
 
-const Home = () => <div><h2>Home</h2></div>
-const About = () => <div><h2>About</h2></div>
 
 export default App;
