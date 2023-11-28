e92383308dcd98eb371cee43102f82948969e52b
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-11-28 13:53:19 +0200

    17

---
+++ src/Components/Homepage.js
@@ -0,0 +1,13 @@
+
+function Vocal() {
+  const expressions = [['Merg la școală. ', ' I am going to school.'], ['Am mâncat la un restaurant. ', ' I ate at a restaurant.'], ['Voi citi o carte. ', ' I will read a book.']];
+
+
+  return (
+    <div className="App">
+      <h1>Language Practice App</h1>
+    </div>
+  );
+}
+
+export default Vocal;
